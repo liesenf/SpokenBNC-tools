8c032491969f3df40b171cded358118d74dcf756
Andreas Liesenfeld <chenliesenfeld@gmail.com>
2020-06-10 09:34:52 +0800

    Add files via upload

---
+++ batch_xml_to_csv_simplified.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+This script batch extracts content from xml files and writes it to csv files.
+Built for SpokenBNC corpus data (http://corpora.lancs.ac.uk/bnc2014/).
+Simplified version extracts only text from utterances ("body/u").
+Complete version extracts all content of utterances ("body/u"), including content from nested texts such as "vocal", "pause", "anon", "event", "unclear".
+See corpus annotation manual for complete tag list.
+
+Created June 2020 at HK PolyU
+@author Andreas Liesenfeld
+"""
+
+import xml.etree.cElementTree as et
+import pandas as pd
+import os
+
+# input and output paths
+path = '/home/andreas/Desktop/SpokenBNC/spoken/untagged'
+path_to_output = r'/home/andreas/Desktop/SpokenBNC/spoken/csv_simple/'
+
+# read files in path directory
+for filename in os.listdir(path):
+    if not filename.endswith('.xml'): continue # read only xml files
+    fullname = os.path.join(path, filename)
+    parsed_xml = et.parse(fullname)
+
+    # create empty dataframe with three columns for extracted content
+    dfcols = ['UID', 'speaker', 'utterance']
+    df_xml = pd.DataFrame(columns=dfcols)
+
+    # extract specified content from xml file:
+    # utterance ID as UID, speaker ID as speaker, utterance content as utterance
+    for turn in parsed_xml.findall('body/u'):
+        uid = turn.attrib.get('n')
+        speaker = turn.attrib.get('who')
+        utterance = "".join(turn.itertext())
+        # append extracted content to dataframe
+        df_xml = df_xml.append(
+            pd.Series([uid, speaker, utterance], index=dfcols),
+            ignore_index=True)
+        # write each dataframe to path_to_output as separate csv file
+        df_xml.to_csv(path_to_output + filename+'.csv', index=False)
+
+    print("Conversion complete: " + filename)
+print("Done")
